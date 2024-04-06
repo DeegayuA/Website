51253497afee204bd2ef11f89957b60170e4bffc
Deeghayu Adhikari <63475473+DeegayuA@users.noreply.github.com>
2024-04-06 14:43:28 +0530

    color updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ window.addEventListener('scroll', scrollActive)
 /*===== CHANGE BACKGROUND HEADER =====*/
 function scrollHeader() {
   const header = document.getElementById('header')
-  if (this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header'); S
+  if (this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header'); S
 }
 window.addEventListener('scroll', scrollHeader)
 
